b39cf4478a4ef5d6f2c02c48cda63b6fd4f4c30c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-08-11 23:48:41 -0700

    Replace CSS resource with static serving and middleware

--- app.ts
+++ app.ts
@@ -1,16 +1,18 @@
-import { exists } from "https://deno.land/std@0.104.0/fs/mod.ts";
-import { join } from "https://deno.land/std@0.104.0/node/path.ts";
+import {
+  dirname,
+  fromFileUrl,
+} from "https://deno.land/std@0.104.0/node/path.ts";
 import { Drash } from "https://deno.land/x/drash@v1.5.1/mod.ts";
 
-function generateGenericRoute(
-  prefix: string,
-  depth: number,
-): string[] {
-  if (depth === 0) return [prefix];
-  const arr = generateGenericRoute(prefix, depth - 1);
-  return arr.concat(
-    `${arr![arr!.length - 1]}/:f${depth}?`,
-  );
+async function RerenderCSSMiddleware(
+  req: Drash.Http.Request,
+): Promise<void> {
+  if (req.url_path.startsWith("css")) {
+    const renderProcess = Deno.run({
+      cmd: ["sass", "scss:www/css"],
+    });
+    await renderProcess.status();
+  }
 }
 
 class HTMLResource extends Drash.Http.Resource {
@@ -22,44 +24,19 @@ class HTMLResource extends Drash.Http.Resource {
   }
 }
 
-class CSSResource extends Drash.Http.Resource {
-  static paths = generateGenericRoute("/css", 32);
-  public async GET() {
-    // Reconstruct the file name from the path parameters
-    const fileName = Object.values(this.request.getAllPathParams()).filter(
-      (param) => param != null && param !== "",
-    ).join(
-      "/",
-    );
-
-    // Rerender all SCSS files
-    const renderProcess = Deno.run({
-      cmd: ["sass", "scss:www/css"],
-    });
-    await renderProcess.status();
-
-    const fsFileName = join("./www/css", fileName);
-
-    // Ensure requested file exists
-    if (fileName == null || fileName === "" || !await exists(fsFileName)) {
-      this.response.status_code = 404;
-      this.response.body = "Not Found";
-      return this.response;
-    }
-
-    // Retrieve the appropriate CSS file
-    this.response.headers.set("Content-Type", "text/css");
-    this.response.body = await Deno.readFile(fsFileName);
-    return this.response;
-  }
-}
-
 const server = new Drash.Http.Server({
+  directory: dirname(fromFileUrl(import.meta.url)),
   logger: new Drash.CoreLoggers.ConsoleLogger({
     enabled: true,
     level: "debug",
   }),
-  resources: [HTMLResource, CSSResource],
+  middleware: {
+    before_request: [RerenderCSSMiddleware],
+  },
+  resources: [HTMLResource],
+  static_paths: {
+    "/css": "/www/css",
+  },
 });
 
 server.run({
