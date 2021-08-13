23ad49813cd249e831965f941337b5598249fb6a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-08-12 23:42:16 -0700

    Add application config

---
+++ app_config.ts
@@ -0,0 +1,4 @@
+export default {
+  COPY_CSV_URL:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTvtfoUIY4-dOjTutKA6bixv9oQrt4s8eAIi8a-e9BLt7f42E4MI9XolF55PYXnjzAfltAOBVY3oCg2/pub?gid=0&single=true&output=csv",
+};
